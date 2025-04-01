db683918df701aac141154481482d96216c26a45
MedhiniCU <141148488+MedhiniCU@users.noreply.github.com>
2025-04-01 12:38:49 +0530

    Update HSR.js

--- HSR.js
+++ HSR.js
@@ -1,1 +1,28 @@
+<script>
+        function toggleDarkMode() {
+            document.body.classList.toggle('dark-mode');
+        }
 
+        function searchItems() {
+            let input = document.getElementById("searchBar").value.toLowerCase();
+            let lists = document.querySelectorAll("ul li");
+
+            lists.forEach(item => {
+                let text = item.innerText.toLowerCase();
+                if (text.includes(input)) {
+                    item.style.display = "flex";
+                } else {
+                    item.style.display = "none";
+                }
+            });
+        }
+
+        function toggleSection(sectionId) {
+            let section = document.getElementById(sectionId);
+            if (section.style.display === "none" || section.style.display === "") {
+                section.style.display = "block";
+            } else {
+                section.style.display = "none";
+            }
+        }
+    </script>
